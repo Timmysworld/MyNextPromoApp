60df75c150efde5a7bc2a50addaedab0909b83fa
TimothySingleton <t.singleton89@hotmail.com>
2023-03-28 20:19:20 -0600

    spacing

--- flask_app/controllers/employees.py
+++ flask_app/controllers/employees.py
@@ -67,6 +67,8 @@ def create_employee():
 def view_employee(id):
     OneEmployee = employee.Employee.get_employee(id)
     return render_template('show_employee.html', employee = OneEmployee)
+
+
 #UPDATE EMPLOYEE:
 
 
